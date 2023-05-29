15f0d334bf4d5460334eaec32e059cde6a1b3320
lesismal <lesismal@gmail.com>
2023-05-29 15:36:07 +0000

    update script

--- script/servers.sh
+++ script/servers.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. ./script/util.sh
+
 echo "run each server on cpu 0-$want_cpu_num"
 # start all servers together, else it would hard to bind addr and start failed after some benchmark
 for f in ${frameworks[@]}; do
