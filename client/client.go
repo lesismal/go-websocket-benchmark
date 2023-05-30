6f573ccc2bad72f9c4140f297bbd530c7f1983aa
lesismal <lesismal@gmail.com>
2023-05-30 19:54:06 +0800

    add simple report

--- client/client.go
+++ client/client.go
@@ -236,7 +236,7 @@ MEM MIN  : %v
 		perf.I2MemString(psCounter.MEMRSSAvg()),
 		perf.I2MemString(psCounter.MEMRSSMax()))
 
-	report := &Report{
+	report := &FullReport{
 		Framework: *framework,
 		Total:     int64(calculator.Total),
 		Success:   calculator.Success,
@@ -270,106 +270,60 @@ MEM MIN  : %v
 }
 
 func makeReport(typ string) {
-	makeReportMarkdown()
+	time.AfterFunc(time.Second*20, func() {
+		os.Exit(-1)
+	})
+	makeReportMarkdown(true)
+	makeReportMarkdown(false)
 }
 
-func makeReportMarkdown() {
-	reports := make([]*Report, len(conf.FrameworkList))
+func makeReportMarkdown(simple bool) {
+	reports := make([]Report, len(conf.FrameworkList))
 	for i, v := range conf.FrameworkList {
 		b, err := os.ReadFile("./output/report/" + v + " .json")
 		if err != nil {
 			log.Fatalf("Read Report %v failed: %v", v, err)
 		}
-		report := &Report{}
+
+		report := &FullReport{}
 		err = json.Unmarshal(b, report)
 		if err != nil {
 			log.Fatalf("Unmarshal Report %v failed: %v", v, err)
 		}
-		reports[i] = report
+		if simple {
+			reports[i] = report.ToSimple()
+		} else {
+			reports[i] = report
+		}
 	}
+
 	table := perf.NewTable()
-	table.SetTitle((&Report{}).Headers())
+	if simple {
+		table.SetTitle((&SimpleReport{}).Headers())
+	} else {
+		table.SetTitle((&FullReport{}).Headers())
+	}
+
 	for _, v := range reports {
 		table.AddRow(v.Strings())
 	}
+
 	text := table.Markdown()
 	fmt.Println(text)
-	err := os.WriteFile("./output/report/report.md", []byte(text), 0666)
-	if err != nil {
-		log.Fatalf("Write Report failed: %v", err)
+	if simple {
+		err := os.WriteFile("./output/report/report_simple.md", []byte(text), 0666)
+		if err != nil {
+			log.Fatalf("Write Report failed: %v", err)
+		}
+	} else {
+		err := os.WriteFile("./output/report/report_full.md", []byte(text), 0666)
+		if err != nil {
+			log.Fatalf("Write Report failed: %v", err)
+		}
 	}
 }
 
-type Report struct {
-	Framework string
-	Total     int64
-	Success   int64
-	Failed    int64
-	TimeUsed  time.Duration
-	Min       int64
-	Avg       int64
-	Max       int64
-	TPS       int64
-	TP50      int64
-	TP75      int64
-	TP90      int64
-	TP95      int64
-	TP99      int64
-	CPUMin    float64
-	CPUAvg    float64
-	CPUMax    float64
-	MEMRSSMin uint64
-	MEMRSSAvg uint64
-	MEMRSSMax uint64
-}
-
-func (r *Report) Headers() []string {
-	ret := [20]string{
-		"Framework",
-		"Total",
-		"Success",
-		"Failed",
-		"TimeUsed",
-		"Min",
-		"Avg",
-		"Max",
-		"TPS",
-		"TP50",
-		"TP75",
-		"TP90",
-		"TP95",
-		"TP99",
-		"CPU Min",
-		"CPU Avg",
-		"CPU Max",
-		"MEM Min",
-		"MEM Avg",
-		"MEM Max",
-	}
-	return ret[:]
-}
-
-func (r *Report) Strings() []string {
-	ret := make([]string, 20)[:0]
-	ret = append(ret, r.Framework)
-	ret = append(ret, fmt.Sprintf("%d", r.Total))
-	ret = append(ret, fmt.Sprintf("%v", r.Success))
-	ret = append(ret, fmt.Sprintf("%v", r.Failed))
-	ret = append(ret, fmt.Sprintf("%v", perf.I2TimeString(int64(r.TimeUsed))))
-	ret = append(ret, fmt.Sprintf("%v", perf.I2TimeString(r.Min)))
-	ret = append(ret, fmt.Sprintf("%v", perf.I2TimeString(r.Avg)))
-	ret = append(ret, fmt.Sprintf("%v", perf.I2TimeString(r.Max)))
-	ret = append(ret, fmt.Sprintf("%v", r.TPS))
-	ret = append(ret, fmt.Sprintf("%v", r.TP50))
-	ret = append(ret, fmt.Sprintf("%v", r.TP75))
-	ret = append(ret, fmt.Sprintf("%v", r.TP90))
-	ret = append(ret, fmt.Sprintf("%v", r.TP95))
-	ret = append(ret, fmt.Sprintf("%v", r.TP99))
-	ret = append(ret, fmt.Sprintf("%.2f%%", r.CPUMin))
-	ret = append(ret, fmt.Sprintf("%.2f%%", r.CPUAvg))
-	ret = append(ret, fmt.Sprintf("%.2f%%", r.CPUMax))
-	ret = append(ret, fmt.Sprintf("%v", perf.I2MemString(r.MEMRSSMin)))
-	ret = append(ret, fmt.Sprintf("%v", perf.I2MemString(r.MEMRSSAvg)))
-	ret = append(ret, fmt.Sprintf("%v", perf.I2MemString(r.MEMRSSMax)))
-	return ret
+type Report interface {
+	Headers() []string
+	Strings() []string
 }
