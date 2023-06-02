3b09a5ddb3b6a30d91bd270fa783d4142e24dc18
lesismal <lesismal@gmail.com>
2023-06-02 14:24:39 +0000

    update script log

--- script/clients.sh
+++ script/clients.sh
@@ -3,11 +3,13 @@
 . ./script/env.sh
 
 for f in ${frameworks[@]}; do
-    # echo "start bench ${f}" $1 $2 $3 $4 $5 $6 $7 $8 $9
     echo
-    #spid=$(pidof ${f}.server)
-    #./script/client.sh -f=$f -spid=$spid $1 $2 $3 $4 $5 $6 $7 $8 $9
-    ./script/client.sh -f=$f $1 $2 $3 $4 $5 $6 $7 $8 $9
+    # echo "start bench ${f}" $1 $2 $3 $4 $5 $6 $7 $8 $9
+    echo "run client to ${f}, on cpu ${client_cpu_num}-$((total_cpu_num - 1))"
+    . ./script/client.sh -f=$f $1 $2 $3 $4 $5 $6 $7 $8 $9
     . ./script/killone.sh "${f}.server"
-    sleep $SleepTime
+    for (( i=1; i<=$SleepTime; i++ )); do
+        echo "sleep $i ..."
+        sleep 1
+    done
 done
