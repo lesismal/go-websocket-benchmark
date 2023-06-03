0617fc124c701e1351bcc34a854e8c4bc8ab72bd
lesismal <lesismal@gmail.com>
2023-06-03 02:08:55 +0000

    update script

--- script/benchmarkN.sh
+++ script/benchmarkN.sh
@@ -21,30 +21,36 @@ echo $line
 
 echo $line
 
+. ./script/killall.sh
+sleep 1
+. ./script/servers.sh
+sleep 1
+echo $line
+for f in ${frameworks[@]}; do
+    echo "run ${f} server on cpu 0-${server_cpu_num}"
+    # nohup $limit_cpu_server "./output/bin/${f}.server" -b=$b >"./output/log/${f}${suffix}.log" 2>&1 &
+    for c in ${Connections[@]}; do
+        for b in ${BodySize[@]}; do
+            for n in ${BenchTime[@]}; do
+                echo $line
+                suffix="_${c}_${b}_${n}"
+                #echo "benchmarkN: [${f}], ${c} connections, ${b} payload, ${n} times"
+                . ./script/client.sh -f=$f -c=$c -b=$b -n=$n -suffix=${suffix}
+                sleep $SleepTime
+            done
+        done
+    done
+    . ./script/killone.sh "${f}.server"
+done
+
 for c in ${Connections[@]}; do
     for b in ${BodySize[@]}; do
         for n in ${BenchTime[@]}; do
+            echo $line
             suffix="_${c}_${b}_${n}"
-            echo $line
-            . ./script/killall.sh
-
-            echo $line
-
-            for f in ${frameworks[@]}; do
-                echo
-                echo "run ${f} server on cpu 0-${server_cpu_num}"
-                nohup $limit_cpu_server "./output/bin/${f}.server" -b=$b >"./output/log/${f}${suffix}.log" 2>&1 &
-            done
-
-            echo $line
-
-            echo "benchmarkN: ${c} connections, ${b} payload, ${n} times"
-            . ./script/clients.sh -c=$c -b=$b -n=$n -suffix=${suffix}
-            echo $line
             . ./script/report.sh -suffix=${suffix} $1 $2 $3 $4 $5 $6 $7 $8 $9
-	    
-            sleep $SleepTime
         done
     done
 done
 echo $line
+
