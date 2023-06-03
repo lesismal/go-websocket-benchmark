1aa9940d8e63616cc7f85e807ab6c4a414bd76b2
lesismal <lesismal@gmail.com>
2023-06-04 00:08:04 +0800

    change benchmark report fields order

--- mwsbench/report/benchecho_report.go
+++ mwsbench/report/benchecho_report.go
@@ -24,12 +24,6 @@ type BenchEchoReport struct {
 	Success     int64   `json:"Success" md:"Success"`
 	Failed      int64   `json:"Failed" md:"Failed"`
 	Used        int64   `json:"Used" md:"Used" fmt:"duration"`
-	CPUMin      float64 `json:"CPUMin" md:"CPU Min" fmt:"cpu"`
-	CPUAvg      float64 `json:"CPUAvg" md:"CPU Avg" fmt:"cpu"`
-	CPUMax      float64 `json:"CPUMax" md:"CPU Max" fmt:"cpu"`
-	MEMRSSMin   uint64  `json:"MEMMin" md:"MEM Min" fmt:"mem"`
-	MEMRSSAvg   uint64  `json:"MEMAvg" md:"MEM Avg" fmt:"mem"`
-	MEMRSSMax   uint64  `json:"MEMMax" md:"MEM Max" fmt:"mem"`
 	TPS         int64   `json:"TPS" md:"TPS"`
 	Min         int64   `json:"Min" md:"Min" fmt:"duration"`
 	Avg         int64   `json:"Avg" md:"Avg" fmt:"duration"`
@@ -39,6 +33,12 @@ type BenchEchoReport struct {
 	TP90        int64   `json:"TP90" md:"TP90" fmt:"duration"`
 	TP95        int64   `json:"TP95" md:"TP95" fmt:"duration"`
 	TP99        int64   `json:"TP99" md:"TP99" fmt:"duration"`
+	CPUMin      float64 `json:"CPUMin" md:"CPU Min" fmt:"cpu"`
+	CPUAvg      float64 `json:"CPUAvg" md:"CPU Avg" fmt:"cpu"`
+	CPUMax      float64 `json:"CPUMax" md:"CPU Max" fmt:"cpu"`
+	MEMRSSMin   uint64  `json:"MEMMin" md:"MEM Min" fmt:"mem"`
+	MEMRSSAvg   uint64  `json:"MEMAvg" md:"MEM Avg" fmt:"mem"`
+	MEMRSSMax   uint64  `json:"MEMMax" md:"MEM Max" fmt:"mem"`
 }
 
 func (r *BenchEchoReport) Type() string {
