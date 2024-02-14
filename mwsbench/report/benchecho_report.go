1b3420b594b303d8a2df8c064a3a923adc229897
lesismal <lesismal@gmail.com>
2024-02-14 18:46:19 +0800

    rm num goroutine

--- mwsbench/report/benchecho_report.go
+++ mwsbench/report/benchecho_report.go
@@ -34,15 +34,15 @@ type BenchEchoReport struct {
 	Connections int     `json:"Conns" md:"Conns"`
 	Concurrency int     `json:"Concurrency" md:"Concurrency"`
 	Payload     int     `json:"Payload" md:"Payload"`
-	GoMin       int     `json:"GoMin" md:"Go Min" fmt:"go"`
-	GoAvg       int     `json:"GoAvg" md:"Go Avg" fmt:"go"`
-	GoMax       int     `json:"GoMax" md:"Go Max" fmt:"go"`
-	CPUMin      float64 `json:"CPUMin" md:"CPU Min" fmt:"cpu"`
-	CPUAvg      float64 `json:"CPUAvg" md:"CPU Avg" fmt:"cpu"`
-	CPUMax      float64 `json:"CPUMax" md:"CPU Max" fmt:"cpu"`
-	MEMRSSMin   uint64  `json:"MEMMin" md:"MEM Min" fmt:"mem"`
-	MEMRSSAvg   uint64  `json:"MEMAvg" md:"MEM Avg" fmt:"mem"`
-	MEMRSSMax   uint64  `json:"MEMMax" md:"MEM Max" fmt:"mem"`
+	// GoMin       int     `json:"GoMin" md:"Go Min" fmt:"go"`
+	// GoAvg       int     `json:"GoAvg" md:"Go Avg" fmt:"go"`
+	// GoMax       int     `json:"GoMax" md:"Go Max" fmt:"go"`
+	CPUMin    float64 `json:"CPUMin" md:"CPU Min" fmt:"cpu"`
+	CPUAvg    float64 `json:"CPUAvg" md:"CPU Avg" fmt:"cpu"`
+	CPUMax    float64 `json:"CPUMax" md:"CPU Max" fmt:"cpu"`
+	MEMRSSMin uint64  `json:"MEMMin" md:"MEM Min" fmt:"mem"`
+	MEMRSSAvg uint64  `json:"MEMAvg" md:"MEM Avg" fmt:"mem"`
+	MEMRSSMax uint64  `json:"MEMMax" md:"MEM Max" fmt:"mem"`
 }
 
 func (r *BenchEchoReport) Type() string {
--- mwsbench/report/benchrate_report.go
+++ mwsbench/report/benchrate_report.go
@@ -19,15 +19,15 @@ type BenchRateReport struct {
 	Connections int     `json:"Conns" md:"Conns"`
 	SendRate    int     `json:"SendRate" md:"SendRate"`
 	Payload     int     `json:"Payload" md:"Payload"`
-	GoMin       int     `json:"GoMin" md:"Go Min" fmt:"go"`
-	GoAvg       int     `json:"GoAvg" md:"Go Avg" fmt:"go"`
-	GoMax       int     `json:"GoMax" md:"Go Max" fmt:"go"`
-	CPUMin      float64 `json:"CPUMin" md:"CPU Min" fmt:"cpu"`
-	CPUAvg      float64 `json:"CPUAvg" md:"CPU Avg" fmt:"cpu"`
-	CPUMax      float64 `json:"CPUMax" md:"CPU Max" fmt:"cpu"`
-	MEMRSSMin   uint64  `json:"MEMMin" md:"MEM Min" fmt:"mem"`
-	MEMRSSAvg   uint64  `json:"MEMAvg" md:"MEM Avg" fmt:"mem"`
-	MEMRSSMax   uint64  `json:"MEMMax" md:"MEM Max" fmt:"mem"`
+	// GoMin       int     `json:"GoMin" md:"Go Min" fmt:"go"`
+	// GoAvg       int     `json:"GoAvg" md:"Go Avg" fmt:"go"`
+	// GoMax       int     `json:"GoMax" md:"Go Max" fmt:"go"`
+	CPUMin    float64 `json:"CPUMin" md:"CPU Min" fmt:"cpu"`
+	CPUAvg    float64 `json:"CPUAvg" md:"CPU Avg" fmt:"cpu"`
+	CPUMax    float64 `json:"CPUMax" md:"CPU Max" fmt:"cpu"`
+	MEMRSSMin uint64  `json:"MEMMin" md:"MEM Min" fmt:"mem"`
+	MEMRSSAvg uint64  `json:"MEMAvg" md:"MEM Avg" fmt:"mem"`
+	MEMRSSMax uint64  `json:"MEMMax" md:"MEM Max" fmt:"mem"`
 }
 
 func (r *BenchRateReport) Type() string {
