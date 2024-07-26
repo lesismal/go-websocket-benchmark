8852933a2da516119a26ec6b83d95ab65b1d3299
lesismal <lesismal@gmail.com>
2024-07-26 21:32:32 +0800

    fix script

--- script/servers.sh
+++ script/servers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # . ./script/env.sh
- . ./script/config.sh
+# . ./script/config.sh
 
 # start all servers together, else it would hard to bind addr and start failed after some benchmark
 for f in ${frameworks[@]}; do
